3afbd8a69096254d8e1296ed78b0ec757a3ab821
beymistic <tomislav.dm@gmail.com>
2018-04-24 15:04:40 +0200

    Task3

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -17,10 +17,20 @@ namespace ContactManager.Controllers
         {
             this.contactRepository = new ContactRepository();
         } 
-
+        //
         public Contact[] Get()
         {
             return contactRepository.GetAllContacts();
         }
+        //
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+
     }
 }
